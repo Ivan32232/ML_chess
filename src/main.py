e39d6c0a82a36609bdbec5ab4fa37953291dc1e1
Ivan32232 <pokhvalitovvanya@gmail.com>
2025-08-11 18:32:31 +0200

    pawn promotion 1

--- src/main.py
+++ src/main.py
@@ -23,8 +23,10 @@ class Main:
         while True:
             # show methods 
             game.show_bg(screen)
+            game.show_last_move(screen)
             game.show_moves(screen)
             game.show_pieces(screen)
+            game.show_hover(screen)
 
             if dragger.dragging:
                 dragger.update_blit(screen)
@@ -46,19 +48,26 @@ class Main:
                             dragger.save_initial(event.pos)
                             dragger.drag_piece(piece)
                             # show methods
-                            game.show_bg(screen)
+                            game.show_bg(screen) 
+                            game.show_last_move(screen)
                             game.show_moves(screen)
                             game.show_pieces(screen)
 
                 # mouse motion 
                 elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+                    game.set_hover(motion_row, motion_col)
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         # show methods
                         game.show_bg(screen)
-                        game.show_moves(screen)
-                        dragger.update_blit(screen)
+                        game.show_moves(screen) 
+                        game.show_last_move(screen)
                         game.show_pieces(screen)
+                        game.show_hover(screen)
+                        dragger.update_blit(screen)
 
                 # click release
                 elif event.type == pygame.MOUSEBUTTONUP:
@@ -77,6 +86,7 @@ class Main:
                             board.move(dragger.piece, move)
                             # draw show methods
                             game.show_bg(screen)
+                            game.show_last_move(screen)
                             game.show_pieces(screen)
                             # next turn
                             game.next_turn()
@@ -84,6 +94,15 @@ class Main:
                     dragger.undrag_piece()
 
                 # quit app
+                elif event.type == pygame.KEYDOWN:
+
+                    # restart game 
+                    if event.key == pygame.K_r:
+                        game.reset()
+                        game = self.game
+                        board = self.game.board
+                        dragger = self.game.dragger
+
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
